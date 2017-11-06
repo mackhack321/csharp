97d6d5516a730ce2ec2d9a5a5e5bf39671468aa2
Mack Stanley <mlgprohacker@gmail.com>
2017-11-06 11:12:00 -0600

    Added forLoopTesting project, .gitattributes came too

---
+++ forLoopTesting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forLoopTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = new int[] { 0, 1, 2, 3, 4 };
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);               
+            }
+            Console.ReadLine();
+        }
+    }
+}
