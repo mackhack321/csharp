0caceb5e6e7e6665c9cfd988e3574f806af12764
= <mlgprohacker@gmail.com>
2017-11-06 19:58:13 -0600

    Played with forlooptesting, added math stuff, experimented with function definition

--- forLoopTesting/Program.cs
+++ forLoopTesting/Program.cs
@@ -8,6 +8,14 @@ namespace forLoopTesting
 {
     class Program
     {
+        public static void funcTest(int i)
+        {
+            foreach (int element in new int[] { 1,2,3,4,5})
+            {
+                Console.WriteLine($"{i} to the power of {element} is {Math.Pow(i, element)}");
+            }
+
+        }
         static void Main(string[] args)
         {
             int[] list = new int[] { 0, 1, 2, 3, 4 };
@@ -20,6 +28,9 @@ namespace forLoopTesting
             {
                 Console.WriteLine(i);
             }
+            Console.Write("Give a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            funcTest(num);
             Console.ReadLine();
         }
     }
