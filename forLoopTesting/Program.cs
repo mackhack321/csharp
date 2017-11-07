367bb805de8f397bea092b209c677642edb3090b
Mack Stanley <mlgprohacker@gmail.com>
2017-11-07 08:25:52 -0600

    Added to forlooptesting, played with ranges and while loops

--- forLoopTesting/Program.cs
+++ forLoopTesting/Program.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace forLoopTesting
 {
     class Program
     {
-        public static void funcTest(int i)
+        public static void funcTest(int i, int rang)
         {
-            foreach (int element in new int[] { 1,2,3,4,5})
+            int p = 1;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            while (p<=rang)
             {
-                Console.WriteLine($"{i} to the power of {element} is {Math.Pow(i, element)}");
+                Console.WriteLine($"{i} to the power of {p} is {Math.Pow(i, p)}");
+                p++;
             }
-
+            stopwatch.Stop();
+            TimeSpan time = stopwatch.Elapsed;
+            Console.WriteLine($"That took {time}");
         }
+
         static void Main(string[] args)
         {
             int[] list = new int[] { 0, 1, 2, 3, 4 };
@@ -30,7 +38,9 @@ namespace forLoopTesting
             }
             Console.Write("Give a number: ");
             int num = Convert.ToInt32(Console.ReadLine());
-            funcTest(num);
+            Console.Write("Give a range: ");
+            int rang = Convert.ToInt32(Console.ReadLine());
+            funcTest(num,rang);
             Console.ReadLine();
         }
     }
