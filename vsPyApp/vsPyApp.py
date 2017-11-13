98693182c62e0b69980a1069286ff6c16c7b82c5
Mack Stanley <mlgprohacker@gmail.com>
2017-11-13 08:49:41 -0600

    Played with vsPyApp

--- vsPyApp/vsPyApp.py
+++ vsPyApp/vsPyApp.py
@@ -1,2 +1,7 @@
-for i in range(0,100):
-    print(i)
+try:
+    num1 = int(input("Give a number: "))
+    num2 = int(input("Give another number: "))
+    sum = num1+num2
+    print(f"The sum of {num1} and {num2} is {sum}.")
+except ValueError:
+    print("Bad input!")
