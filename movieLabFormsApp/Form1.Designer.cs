bda35a29db231e2b18f3cabeab0629ab8113a35a
Mack Stanley <mlgprohacker@gmail.com>
2017-11-14 13:54:26 -0600

    turned movieLab into a forms app

---
+++ movieLabFormsApp/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace movieLabFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.ageDropdown = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "IT",
+            "Jaws",
+            "The Spongebob Movie: Sponge Out Of Water",
+            "Killer Joe",
+            "The Lion King",
+            "Thor: Ragnarok"});
+            this.listBox1.Location = new System.Drawing.Point(80, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select a Movie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter your Age";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(104, 199);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 3;
+            this.buttonSubmit.Text = "OK";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // ageDropdown
+            // 
+            this.ageDropdown.FormattingEnabled = true;
+            this.ageDropdown.Location = new System.Drawing.Point(80, 163);
+            this.ageDropdown.Name = "ageDropdown";
+            this.ageDropdown.Size = new System.Drawing.Size(121, 21);
+            this.ageDropdown.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ageDropdown);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Movie Lab";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.ComboBox ageDropdown;
+    }
+}
+
---
+++ movieLabFormsApp/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace movieLabFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (int i in Enumerable.Range(1, 100))
+            {
+                ageDropdown.Items.Add(i);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            IDictionary<string, string> movies = new Dictionary<string, string>
+            {
+                { "IT", "R" },
+                { "Jaws", "PG" },
+                { "The SpongeBob Movie: Sponge Out of Water", "PG" },
+                { "Killer Joe", "NC-17" },
+                { "The Lion King", "G" },
+                { "Thor: Ragnarok", "PG-13" }
+            };
+            try
+            {
+                int age = int.Parse(ageDropdown.Text);
+                string usrMovie = listBox1.Text;
+                string usrRating = movies[usrMovie];
+                if (usrRating == "R" && age < 17)
+                {
+                    MessageBox.Show("You must be accompanied by a parent or guardian to watch this film", "Advisory");
+                }
+                else if (usrRating == "NC-17" && age < 18)
+                {
+                    MessageBox.Show("You are not permitted to watch this film", "Advisory");
+                }
+                else if (usrRating == "PG-13" && age < 13)
+                {
+                    MessageBox.Show("Parents are urged to be cautious of permitting their pre-teen to watch this film", "Advisory");
+                }
+                else
+                {
+                    MessageBox.Show("Enjoy the film", "Notification");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (listBox1.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Please select a movie", "Error");
+                }
+                else if (ageDropdown.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Please enter your age", "Error");
+                }
+            }
+
+        }
+    }
+}
