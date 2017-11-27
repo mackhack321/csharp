21f1338dd96ec139dd3314ca9e641d599a69189e
Mack Stanley <mlgprohacker@gmail.com>
2017-11-27 08:25:05 -0600

    added doWhile, still need to fix condition issue

---
+++ doWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhile
+{
+    class Program
+    {
+        static bool good = false;
+
+        public static void run()
+        {
+            double num = -1;
+            Console.Write("Hey bud.  Give me a number between 1 and 100: ");
+            while (num < 1 || num > 100)
+            {
+                num = double.Parse(Console.ReadLine());
+                good = true;
+                Console.WriteLine($"Your number is {num}.  Press enter to close...");
+                Console.ReadLine();
+            }
+        }
+        static void Main(string[] args)
+        {     
+            while (good is false)
+            {
+                try
+                {
+                    run();
+                }
+                catch (FormatException) { }
+            }
+        }
+    }
+}
