9a8b7f41bee298d1912fa3cd228f3b6ad2eacbc3
Mack Stanley <mlgprohacker@gmail.com>
2017-12-04 08:58:03 -0600

    did arraySandbox

---
+++ arraySandbox/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraySandbox
+{
+    class arrayClass
+    {
+        public static int[] array = new int[10];
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Give an int to fill index {i}: ");
+                try
+                {
+                    arrayClass.array[i] = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    i--;
+                }
+            }
+            Console.WriteLine(arrayClass.array);
+            Console.ReadLine();
+        }
+    }
+}
