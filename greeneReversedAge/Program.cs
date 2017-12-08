0653de06fded6f0bb738b9fea3396acecc28a11c
Mack Stanley <mlgprohacker@gmail.com>
2017-12-08 08:26:53 -0600

    worked on greene age problem

---
+++ greeneReversedAge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+            “Recently I had a visit with my mom and we realized that the two digits that make up my age when
+            reversed resulted in her age. For example, if she’s 73, I’m 37.We wondered how often this has happened
+            over the years but we got sidetracked with other topics and we never came up with an answer. “When I
+            got home I figured out that the digits of our ages have been reversible six times so far.I also figured out
+            that if we’re lucky it would happen again in a few years, and if we’re really lucky it would happen one
+            more time after that.In other words, it would have happened 8 times over all.So the question is, how old
+            am I now ?”
+            Write a program that searches for solutions to this problem.
+*/
+
+namespace greeneReversedAge
+{
+    class Program
+    {
+        public static string reverse(string s)
+        {
+            char[] ageArray = s.ToCharArray();
+            Array.Reverse(ageArray);
+            return new string (ageArray);
+        }
+
+        static void Main(string[] args)
+        {
+            string myAge = "0";
+            string momAge = "36";
+            for (int i = 0; i < 1000; i++)
+            {
+                if (reverse(myAge) == momAge)
+                {
+                    Console.WriteLine($"My age is {myAge} and mom's age is {momAge}");
+                }
+                else
+                {
+                    Console.WriteLine($"{myAge} and {momAge} don't work");
+                }
+                int momAgeInt = int.Parse(momAge);
+                int myAgeInt = int.Parse(myAge);
+                myAge = (myAgeInt + 1).ToString();
+                momAge = (momAgeInt + 1).ToString();
+            }
+            Console.ReadLine();
+        }
+    }
+}
