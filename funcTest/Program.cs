6518d9b7fb415b0f3ab9ae5911e7058fea50b604
Mack Stanley <mlgprohacker@gmail.com>
2017-12-12 07:36:26 -0600

    did funcTest

---
+++ funcTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcTest
+{
+    class Program
+    {
+        static bool checkIfAlpha(string input)
+        {
+            if (input.All(Char.IsLetter)){ return true; }
+            else { return false; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give an input: ");
+            string input = Console.ReadLine().ToString();
+            if (checkIfAlpha(input)) { Console.WriteLine("Function returned true"); }
+            else { Console.WriteLine("Function returned false"); }
+            Console.ReadLine();
+        }
+    }
+}
