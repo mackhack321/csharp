b9ed762de82358e892967920e99ae3110d02fbb4
Mack Stanley <mlgprohacker@gmail.com>
2017-12-18 10:09:31 -0600

    did greeneMidterm c# programs

---
+++ greeneMidtermDigits/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Halfway through writing this, I realize that this is the one that's mindlessly easy in Python.
+//Oh well, my C# kung-fu seems to be strong enough to handle it
+
+namespace greeneMidtermDigits
+{
+    class Program
+    {
+        public static char[] toList(double number)
+        {
+            string strNumber = number.ToString();
+            if (strNumber.Contains(".")) { strNumber = strNumber.Replace(".", "");}
+            if (strNumber.Contains("-")) { strNumber = strNumber.Replace("-", ""); }
+            char[] chars = strNumber.ToCharArray();
+            return chars; 
+        }
+
+        static void Main(string[] args)
+        {
+            bool valid = false;
+            while (valid is false)
+            {
+                try
+                {
+                    Console.Write("Give a number to turn into a list of digits: ");
+                    double num = double.Parse(Console.ReadLine());
+                    if (num.ToString().Length > 15) { Console.WriteLine("Numbers that big make me sad :("); throw new FormatException(); }
+                    valid = true;
+                    Console.Write("Here is a list of the digits in that number: ");
+                    Console.Write(string.Join(", ", toList(num)));
+                    Console.ReadLine();
+                }
+                catch (FormatException) { continue; }
+            }
+        }
+    }
+}
---
+++ greeneMidtermLoseit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greeneMidtermLoseit
+{
+    class Program
+    {
+        public static List<int> nums = new List<int> { 1, 2, 3, 4, 5 };
+
+        public static List<int> loseit(List<int> list)
+        {
+            list.Remove(list[0]);
+            list.Remove(list[list.Count - 1]);
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Original List: "); Console.WriteLine(string.Join(", ", nums));
+            Console.Write("After loseit(): "); Console.Write(string.Join(", ", loseit(nums)));
+            Console.ReadLine();
+        }
+    }
+}
