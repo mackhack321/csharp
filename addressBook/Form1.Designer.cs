493248de9e8ebda052790a8dd2a2a75946ab799e
Mack Stanley <mlgprohacker@gmail.com>
2017-12-19 14:45:03 -0600

    began C# address book

---
+++ addressBook/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace addressBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFName = new System.Windows.Forms.TextBox();
+            this.labelFName = new System.Windows.Forms.Label();
+            this.textBoxLName = new System.Windows.Forms.TextBox();
+            this.labelLName = new System.Windows.Forms.Label();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.comboBoxAge = new System.Windows.Forms.ComboBox();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxFName
+            // 
+            this.textBoxFName.Location = new System.Drawing.Point(94, 37);
+            this.textBoxFName.Name = "textBoxFName";
+            this.textBoxFName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFName.TabIndex = 0;
+            // 
+            // labelFName
+            // 
+            this.labelFName.AutoSize = true;
+            this.labelFName.Location = new System.Drawing.Point(116, 21);
+            this.labelFName.Name = "labelFName";
+            this.labelFName.Size = new System.Drawing.Size(57, 13);
+            this.labelFName.TabIndex = 1;
+            this.labelFName.Text = "First Name";
+            // 
+            // textBoxLName
+            // 
+            this.textBoxLName.Location = new System.Drawing.Point(237, 37);
+            this.textBoxLName.Name = "textBoxLName";
+            this.textBoxLName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLName.TabIndex = 2;
+            // 
+            // labelLName
+            // 
+            this.labelLName.AutoSize = true;
+            this.labelLName.Location = new System.Drawing.Point(262, 21);
+            this.labelLName.Name = "labelLName";
+            this.labelLName.Size = new System.Drawing.Size(58, 13);
+            this.labelLName.TabIndex = 3;
+            this.labelLName.Text = "Last Name";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(300, 91);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 4;
+            this.buttonSubmit.Text = "Add";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.firstName,
+            this.lastName,
+            this.phone,
+            this.age});
+            this.dataGridView1.Location = new System.Drawing.Point(119, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 213);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(370, 37);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPhone.TabIndex = 6;
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(381, 21);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(78, 13);
+            this.labelPhone.TabIndex = 7;
+            this.labelPhone.Text = "Phone Number";
+            // 
+            // comboBoxAge
+            // 
+            this.comboBoxAge.FormattingEnabled = true;
+            this.comboBoxAge.Location = new System.Drawing.Point(511, 36);
+            this.comboBoxAge.Name = "comboBoxAge";
+            this.comboBoxAge.Size = new System.Drawing.Size(48, 21);
+            this.comboBoxAge.TabIndex = 8;
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(523, 21);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(26, 13);
+            this.labelAge.TabIndex = 9;
+            this.labelAge.Text = "Age";
+            // 
+            // firstName
+            // 
+            this.firstName.HeaderText = "First Name";
+            this.firstName.Name = "firstName";
+            this.firstName.ReadOnly = true;
+            // 
+            // lastName
+            // 
+            this.lastName.HeaderText = "Last Name";
+            this.lastName.Name = "lastName";
+            this.lastName.ReadOnly = true;
+            // 
+            // phone
+            // 
+            this.phone.HeaderText = "Phone";
+            this.phone.Name = "phone";
+            this.phone.ReadOnly = true;
+            // 
+            // age
+            // 
+            this.age.HeaderText = "Age";
+            this.age.Name = "age";
+            this.age.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 417);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.comboBoxAge);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.labelLName);
+            this.Controls.Add(this.textBoxLName);
+            this.Controls.Add(this.labelFName);
+            this.Controls.Add(this.textBoxFName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFName;
+        private System.Windows.Forms.Label labelFName;
+        private System.Windows.Forms.TextBox textBoxLName;
+        private System.Windows.Forms.Label labelLName;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.ComboBox comboBoxAge;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn age;
+    }
+}
+
---
+++ addressBook/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace addressBook
+{
+    public partial class Form1 : Form
+    {
+        class data
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string age { get; set; }
+            public string phone { get; set; }
+        }
+
+        data[] dictionary = new data[100];
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 101; i++) { comboBoxAge.Items.Add(i); }
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            if (textBoxFName.Text is "" || textBoxLName.Text is "" || textBoxPhone.Text is "" || comboBoxAge.Text is "")
+            {
+                MessageBox.Show("Please fill in all fields", "Error");
+            }
+
+            else
+            {
+                for (int i = 0; i < 101; i++)
+                {
+                    if (dictionary[i] is null)
+                    {
+                        dictionary[i] = new data
+                        {
+                            firstName = textBoxFName.Text,
+                            lastName = textBoxLName.Text,
+                            age = comboBoxAge.Text,
+                            phone = textBoxPhone.Text
+                        };
+                        dataGridView1.Rows.Add(dictionary[i].firstName, dictionary[i].lastName, dictionary[i].phone, dictionary[i].age);
+                        textBoxFName.Clear(); textBoxLName.Clear(); textBoxPhone.Clear(); comboBoxAge.Text = "";
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
