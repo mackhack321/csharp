63abd7e6e95102d5f133d4f0f4c83ee9ac3cb5bd
Mack Stanley <mlgprohacker@gmail.com>
2018-03-28 21:50:21 -0500

    did tic tac toe forms app

---
+++ ticTacToe/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ticTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonTopLt = new System.Windows.Forms.Button();
+            this.buttonTopMid = new System.Windows.Forms.Button();
+            this.buttonTopRt = new System.Windows.Forms.Button();
+            this.buttonMidLt = new System.Windows.Forms.Button();
+            this.buttonMidMid = new System.Windows.Forms.Button();
+            this.buttonMidRt = new System.Windows.Forms.Button();
+            this.buttonBotLt = new System.Windows.Forms.Button();
+            this.buttonBotMid = new System.Windows.Forms.Button();
+            this.buttonBotRt = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(57, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(678, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Super Crazy C# Tic-Tac-Toe";
+            // 
+            // buttonTopLt
+            // 
+            this.buttonTopLt.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTopLt.Location = new System.Drawing.Point(224, 112);
+            this.buttonTopLt.Name = "buttonTopLt";
+            this.buttonTopLt.Size = new System.Drawing.Size(94, 82);
+            this.buttonTopLt.TabIndex = 1;
+            this.buttonTopLt.Text = "Z";
+            this.buttonTopLt.UseVisualStyleBackColor = true;
+            this.buttonTopLt.Click += new System.EventHandler(this.buttonTopLt_Click);
+            // 
+            // buttonTopMid
+            // 
+            this.buttonTopMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTopMid.Location = new System.Drawing.Point(335, 112);
+            this.buttonTopMid.Name = "buttonTopMid";
+            this.buttonTopMid.Size = new System.Drawing.Size(94, 82);
+            this.buttonTopMid.TabIndex = 2;
+            this.buttonTopMid.Text = "Z";
+            this.buttonTopMid.UseVisualStyleBackColor = true;
+            this.buttonTopMid.Click += new System.EventHandler(this.buttonTopMid_Click);
+            // 
+            // buttonTopRt
+            // 
+            this.buttonTopRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTopRt.Location = new System.Drawing.Point(456, 112);
+            this.buttonTopRt.Name = "buttonTopRt";
+            this.buttonTopRt.Size = new System.Drawing.Size(94, 82);
+            this.buttonTopRt.TabIndex = 3;
+            this.buttonTopRt.Text = "Z";
+            this.buttonTopRt.UseVisualStyleBackColor = true;
+            this.buttonTopRt.Click += new System.EventHandler(this.buttonTopRt_Click);
+            // 
+            // buttonMidLt
+            // 
+            this.buttonMidLt.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMidLt.Location = new System.Drawing.Point(224, 211);
+            this.buttonMidLt.Name = "buttonMidLt";
+            this.buttonMidLt.Size = new System.Drawing.Size(94, 82);
+            this.buttonMidLt.TabIndex = 4;
+            this.buttonMidLt.Text = "Z";
+            this.buttonMidLt.UseVisualStyleBackColor = true;
+            this.buttonMidLt.Click += new System.EventHandler(this.buttonMidLt_Click);
+            // 
+            // buttonMidMid
+            // 
+            this.buttonMidMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMidMid.Location = new System.Drawing.Point(335, 211);
+            this.buttonMidMid.Name = "buttonMidMid";
+            this.buttonMidMid.Size = new System.Drawing.Size(94, 82);
+            this.buttonMidMid.TabIndex = 5;
+            this.buttonMidMid.Text = "Z";
+            this.buttonMidMid.UseVisualStyleBackColor = true;
+            this.buttonMidMid.Click += new System.EventHandler(this.buttonMidMid_Click);
+            // 
+            // buttonMidRt
+            // 
+            this.buttonMidRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMidRt.Location = new System.Drawing.Point(456, 211);
+            this.buttonMidRt.Name = "buttonMidRt";
+            this.buttonMidRt.Size = new System.Drawing.Size(94, 82);
+            this.buttonMidRt.TabIndex = 6;
+            this.buttonMidRt.Text = "Z";
+            this.buttonMidRt.UseVisualStyleBackColor = true;
+            this.buttonMidRt.Click += new System.EventHandler(this.buttonMidRt_Click);
+            // 
+            // buttonBotLt
+            // 
+            this.buttonBotLt.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBotLt.Location = new System.Drawing.Point(224, 320);
+            this.buttonBotLt.Name = "buttonBotLt";
+            this.buttonBotLt.Size = new System.Drawing.Size(94, 82);
+            this.buttonBotLt.TabIndex = 7;
+            this.buttonBotLt.Text = "Z";
+            this.buttonBotLt.UseVisualStyleBackColor = true;
+            this.buttonBotLt.Click += new System.EventHandler(this.buttonBotLt_Click);
+            // 
+            // buttonBotMid
+            // 
+            this.buttonBotMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBotMid.Location = new System.Drawing.Point(335, 320);
+            this.buttonBotMid.Name = "buttonBotMid";
+            this.buttonBotMid.Size = new System.Drawing.Size(94, 82);
+            this.buttonBotMid.TabIndex = 8;
+            this.buttonBotMid.Text = "Z";
+            this.buttonBotMid.UseVisualStyleBackColor = true;
+            this.buttonBotMid.Click += new System.EventHandler(this.buttonBotMid_Click);
+            // 
+            // buttonBotRt
+            // 
+            this.buttonBotRt.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBotRt.Location = new System.Drawing.Point(456, 320);
+            this.buttonBotRt.Name = "buttonBotRt";
+            this.buttonBotRt.Size = new System.Drawing.Size(94, 82);
+            this.buttonBotRt.TabIndex = 9;
+            this.buttonBotRt.Text = "Z";
+            this.buttonBotRt.UseVisualStyleBackColor = true;
+            this.buttonBotRt.Click += new System.EventHandler(this.buttonBotRt_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.Red;
+            this.buttonReset.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.ForeColor = System.Drawing.Color.Black;
+            this.buttonReset.Location = new System.Drawing.Point(650, 272);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(122, 51);
+            this.buttonReset.TabIndex = 10;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(650, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 56);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Quit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonBotRt);
+            this.Controls.Add(this.buttonBotMid);
+            this.Controls.Add(this.buttonBotLt);
+            this.Controls.Add(this.buttonMidRt);
+            this.Controls.Add(this.buttonMidMid);
+            this.Controls.Add(this.buttonMidLt);
+            this.Controls.Add(this.buttonTopRt);
+            this.Controls.Add(this.buttonTopMid);
+            this.Controls.Add(this.buttonTopLt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonTopLt;
+        private System.Windows.Forms.Button buttonTopMid;
+        private System.Windows.Forms.Button buttonTopRt;
+        private System.Windows.Forms.Button buttonMidLt;
+        private System.Windows.Forms.Button buttonMidMid;
+        private System.Windows.Forms.Button buttonMidRt;
+        private System.Windows.Forms.Button buttonBotLt;
+        private System.Windows.Forms.Button buttonBotMid;
+        private System.Windows.Forms.Button buttonBotRt;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ticTacToe/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public bool checkForXWin()
+        {
+            return (buttonTopLt.Text == "X" && buttonTopMid.Text == "X" && buttonTopRt.Text == "X") ||
+                   (buttonMidLt.Text == "X" && buttonMidMid.Text == "X" && buttonMidRt.Text == "X") ||
+                   (buttonBotLt.Text == "X" && buttonBotMid.Text == "X" && buttonBotRt.Text == "X") ||
+                   (buttonTopLt.Text == "X" && buttonMidLt.Text == "X" && buttonBotLt.Text == "X") ||
+                   (buttonTopMid.Text == "X" && buttonMidMid.Text == "X" && buttonBotMid.Text == "X") ||
+                   (buttonTopRt.Text == "X" && buttonMidRt.Text == "X" && buttonBotRt.Text == "X") ||
+                   (buttonTopLt.Text == "X" && buttonMidMid.Text == "X" && buttonBotRt.Text == "X") ||
+                   (buttonTopRt.Text == "X" && buttonMidMid.Text == "X" && buttonBotLt.Text == "X");
+        }
+
+        public bool checkForOWin()
+        {
+            return (buttonTopLt.Text == "O" && buttonTopMid.Text == "O" && buttonTopRt.Text == "O") ||
+                   (buttonMidLt.Text == "O" && buttonMidMid.Text == "O" && buttonMidRt.Text == "O") ||
+                   (buttonBotLt.Text == "O" && buttonBotMid.Text == "O" && buttonBotRt.Text == "O") ||
+                   (buttonTopLt.Text == "O" && buttonMidLt.Text == "O" && buttonBotLt.Text == "O") ||
+                   (buttonTopMid.Text == "O" && buttonMidMid.Text == "O" && buttonBotMid.Text == "O") ||
+                   (buttonTopRt.Text == "O" && buttonMidRt.Text == "O" && buttonBotRt.Text == "O") ||
+                   (buttonTopLt.Text == "O" && buttonMidMid.Text == "O" && buttonBotRt.Text == "O") ||
+                   (buttonTopRt.Text == "O" && buttonMidMid.Text == "O" && buttonBotLt.Text == "O");
+        }
+
+        public void reset()
+        {
+            Control[] buttons = new Control[] { buttonTopLt, buttonTopMid, buttonTopRt,
+                                                buttonMidLt, buttonMidMid, buttonMidRt,
+                                                buttonBotLt, buttonBotMid, buttonBotRt};
+            foreach(Control button in buttons)
+            {
+                button.Text = "Z";
+            }
+        }
+
+        public Control getGoodButton()
+        {
+            Control[] buttons = new Control[] { buttonTopLt, buttonTopMid, buttonTopRt,
+                                                buttonMidLt, buttonMidMid, buttonMidRt,
+                                                buttonBotLt, buttonBotMid, buttonBotRt};
+            Random rand = new Random();
+            bool good = false;
+            while (good is false)
+            {
+                int randindex = rand.Next(buttons.Count());
+                Control randbutton = buttons[randindex];
+                if (randbutton.Text != "X" && randbutton.Text != "O") { good = true; return randbutton; }
+            }
+            return null;
+        }
+        
+        public void doAITurn()
+        {
+            getGoodButton().Text = "O";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonTopLt_Click(object sender, EventArgs e)
+        {
+            buttonTopLt.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }                    
+        }
+
+        private void buttonTopMid_Click(object sender, EventArgs e)
+        {
+            buttonTopMid.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!","Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }  
+        }
+
+        private void buttonTopRt_Click(object sender, EventArgs e)
+        {
+            buttonTopRt.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }
+        }
+
+        private void buttonMidLt_Click(object sender, EventArgs e)
+        {
+            buttonMidLt.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }
+        }
+
+        private void buttonMidMid_Click(object sender, EventArgs e)
+        {
+            buttonMidMid.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }
+        }
+
+        private void buttonMidRt_Click(object sender, EventArgs e)
+        {
+            buttonMidRt.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }
+        }
+
+        private void buttonBotLt_Click(object sender, EventArgs e)
+        {
+            buttonBotLt.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }
+            doAITurn(); 
+        }
+
+        private void buttonBotMid_Click(object sender, EventArgs e)
+        {
+            buttonBotMid.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }
+        }
+
+        private void buttonBotRt_Click(object sender, EventArgs e)
+        {
+            buttonBotRt.Text = "X";
+            if (checkForXWin()) { MessageBox.Show("You Win!", "Winner"); reset(); }
+            doAITurn();
+            if (checkForOWin()) { MessageBox.Show("You Lose...", "Loser"); reset(); }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
