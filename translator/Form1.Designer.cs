6218c3727f2217a4e270e395f11edf9b861d476f
Mack Stanley <mlgprohacker@gmail.com>
2018-04-24 13:21:11 -0500

    start translator

---
+++ translator/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.comboBoxInput = new System.Windows.Forms.ComboBox();
+            this.buttonTranslate = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.comboBoxOutput = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(41, 107);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(155, 20);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // comboBoxInput
+            // 
+            this.comboBoxInput.FormattingEnabled = true;
+            this.comboBoxInput.Location = new System.Drawing.Point(57, 67);
+            this.comboBoxInput.Name = "comboBoxInput";
+            this.comboBoxInput.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxInput.TabIndex = 1;
+            this.comboBoxInput.Text = "Input Language";
+            // 
+            // buttonTranslate
+            // 
+            this.buttonTranslate.Location = new System.Drawing.Point(226, 155);
+            this.buttonTranslate.Name = "buttonTranslate";
+            this.buttonTranslate.Size = new System.Drawing.Size(75, 23);
+            this.buttonTranslate.TabIndex = 2;
+            this.buttonTranslate.Text = "Translate";
+            this.buttonTranslate.UseVisualStyleBackColor = true;
+            this.buttonTranslate.Click += new System.EventHandler(this.buttonTranslate_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Enabled = false;
+            this.textBoxOutput.Location = new System.Drawing.Point(325, 107);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(155, 20);
+            this.textBoxOutput.TabIndex = 3;
+            // 
+            // comboBoxOutput
+            // 
+            this.comboBoxOutput.FormattingEnabled = true;
+            this.comboBoxOutput.Location = new System.Drawing.Point(345, 67);
+            this.comboBoxOutput.Name = "comboBoxOutput";
+            this.comboBoxOutput.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxOutput.TabIndex = 4;
+            this.comboBoxOutput.Text = "Output Language";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "True-Value Google Translate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 294);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxOutput);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonTranslate);
+            this.Controls.Add(this.comboBoxInput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.ComboBox comboBoxInput;
+        private System.Windows.Forms.Button buttonTranslate;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.ComboBox comboBoxOutput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ translator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace translator
+{
+    public partial class Form1 : Form
+    {
+        public IDictionary<string, string> languageCodes = new Dictionary<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxInput.DropDownStyle = ComboBoxStyle.DropDownList; // so user cannot type their own input
+            comboBoxOutput.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxInput.Items.Add("English");
+            comboBoxInput.Items.Add("Spanish");
+            comboBoxOutput.Items.Add("English");
+            comboBoxOutput.Items.Add("Spanish");
+            languageCodes.Add("English", "en");
+            languageCodes.Add("Spanish", "es");
+        }
+
+        private void buttonTranslate_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Enabled = true;
+            textBoxOutput.Text = $"Sample Text in {languageCodes[comboBoxOutput.Text]}";
+        }
+    }
+}
